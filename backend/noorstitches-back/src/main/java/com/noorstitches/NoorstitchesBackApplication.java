1cb14b8077605b9bc92e50abcd3d78a95ee380c9
Noor A. S. <nooalosag@alu.edu.gva.es>
2025-04-19 00:36:21 +0200

    ADD: Proyectos creados tanto de back como de front

---
+++ backend/noorstitches-back/src/main/java/com/noorstitches/NoorstitchesBackApplication.java
@@ -0,0 +1,13 @@
+package com.noorstitches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoorstitchesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoorstitchesBackApplication.class, args);
+	}
+
+}

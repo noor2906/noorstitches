e6bb9d7ee8aa7fc333d794a92d40635661ff9b52
Noor A. S. <nooalosag@alu.edu.gva.es>
2025-05-09 00:08:41 +0200

    CREATE(WebConfig.java): Añadir WebConfig para habilitar CORS y permitir la conexión desde el frontend

---
+++ backend/noorstitches-back/src/main/java/com/noorstitches/configuration/WebConfig.java
@@ -0,0 +1,24 @@
+package com.noorstitches.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Se aplica a todos los endpoints
+                        .allowedOrigins("http://localhost:4200") //una vez se suba a Netlify por ejemplo, agregaremos: "https://tu-app.netlify.app"
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}

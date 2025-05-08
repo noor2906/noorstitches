93b499c7084ace6f0e7d8d3b260314c2174f539c
Noor A. S. <nooalosag@alu.edu.gva.es>
2025-05-09 00:14:58 +0200

    FEAT(app component):  configurar rutas y clientes HTTP en la app
    - Se ha configurado el `AppComponent` para incluir el `router-outlet` y manejar la navegación entre páginas.
    - En el `AppConfig`, se ha añadido el `provideHttpClient` para permitir las peticiones HTTP al backend.
    - Se han establecido las rutas necesarias en `AppRoot` para la redirección a las páginas correspondientes.

--- frontend/noorstitches-front/src/app/app.routes.ts
+++ frontend/noorstitches-front/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/login-page/login-page.component').then(m => m.LoginPageComponent)
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./pages/home-page/home-page.component').then(m => m.HomePageComponent)
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./pages/register-page/register-page.component').then(m => m.RegisterPageComponent)
+    },
+];

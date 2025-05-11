9c1284de6e380efa0988533b97facbd113169c7a
Noor A. S. <nooalosag@alu.edu.gva.es>
2025-05-11 19:28:07 +0200

    UPDATE(header component): header fijo arriba

--- frontend/noorstitches-front/src/app/shared/components/header/header.component.ts
+++ frontend/noorstitches-front/src/app/shared/components/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatIconModule, RouterLink],
+  imports: [MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })

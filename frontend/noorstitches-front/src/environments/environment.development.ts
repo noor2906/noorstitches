ad31424891a4329a7ad0800b2c0302aa4df481a9
Noor A. S. <162999081+noor2906@users.noreply.github.com>
2025-06-09 20:01:05 +0200

    UPDATE(configuraciones deployment)

--- frontend/noorstitches-front/src/environments/environment.development.ts
+++ frontend/noorstitches-front/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8888',
+    production: true,
+    apiUrl: 'https://motivated-appreciation-production-531d.up.railway.app',
     PAYPAL_CLIENT_ID: 'AQt60M9kBYy-qjeyx9Mt8LylAXjCraSygPypseurO9JHqtBDqQwiL5XDwNeknJ-J1fTIq9o68eMUtsv2',
     PAYPAL_CLIENT_SECRET: 'EAR_epHA4fDVqtUirLCxg5pISnvpQvKgj6erPHT3E24abqPLBL_vyPl8olnxHxw0SzrNVRsXUrK2-R4g',
     PAYPAL_APIURL: 'https://api-m.sandbox.paypal.com'

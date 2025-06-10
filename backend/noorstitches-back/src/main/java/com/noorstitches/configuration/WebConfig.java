8c760689e7f2788ce57582474f51cd7018a8dd33
Noor A. S. <162999081+noor2906@users.noreply.github.com>
2025-06-10 10:23:22 +0200

    Update WebConfig.java

--- backend/noorstitches-back/src/main/java/com/noorstitches/configuration/WebConfig.java
+++ backend/noorstitches-back/src/main/java/com/noorstitches/configuration/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Se aplica a todos los endpoints
-                        .allowedOrigins("https://noorstitches.netlify.app") //una vez se suba a Netlify por ejemplo, agregaremos: "https://tu-app.netlify.app"
+                registry.addMapping("/**")
+                        .allowedOrigins("https://noorstitches.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
